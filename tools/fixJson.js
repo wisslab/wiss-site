2ff0933f5fb5919196086519e185892817ebac14
Kai Eckert <gen-github@kaiec.de>
2018-02-07 15:57:14 +0100

    Fixed span = nocaps and strange backslashes in json

--- tools/fixJson.js
+++ tools/fixJson.js
@@ -3,6 +3,7 @@ var fs = require("fs")
 var jp = require("jsonpath")
 var contents = fs.readFileSync(process.argv[2], 'utf8');
 contents = contents.replace(/date-parts/g, "year")
+contents = contents.replace(/\\\\/g, "").replace(/<span class=\\\"nocase\\\">/g, "").replace(/<\/span>/g, "")
 data = JSON.parse(contents)
 jp.apply(data, "$[*].issued.year", function(value){return value[0][0]})
 jp.apply(data, "$[*]", function(value){if (!value.issued) {value.issued={year: -1}};return value})
